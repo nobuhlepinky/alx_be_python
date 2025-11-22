94f66534dfab387b360e107e98c7cf28ee1e6bbe
Nobuhle_Pinky <nobuhlepinky11@gmail.com>
2025-11-22 12:39:30 +0200

    Added arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    op = operation.lower()
+
+    if op == 'add':
+        return num1 + num2
+    
+    elif op == 'subtract':
+        return num1 - num2
+    
+    elif op == 'multiply':
+        return num1 * num2
+    elif op == 'divide':
+        
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
